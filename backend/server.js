fad44283101983030211507ad5c15cd720f41cc7
Sujal366 <sujalgandhi786@gmail.com>
2025-02-22 14:04:55 +0530

    mongo connection error fix

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,7 @@ if (!process.env.MONGO_URI) {
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => {
     console.error("❌ MongoDB connection error:", err);
