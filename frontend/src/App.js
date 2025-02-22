579dbadd11ee8e44168c040f9c41fefcd6d55a4b
Sujal366 <sujalgandhi786@gmail.com>
2025-02-22 15:13:43 +0530

    connected backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io(process.env.REACT_APP_BACKEND_URL);
 
 const App = () => {
   const [message, setMessage] = useState("");
